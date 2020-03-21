eafbe607586d29a0a10498b3b1d2a077c2f2a2c0
Trung Truong <ttruong91498@gmail.com>
2020-03-21 12:55:50 -0400

    Add TCP_Select_Server class, add exceptions

--- src/tcp.cpp
+++ src/tcp.cpp
@@ -11,6 +11,145 @@
 #include "tcp.h"
 
 /*===========================================================================
+ * TCP select server implementation
+ *==========================================================================*/
+TCP_Select_Server::TCP_Select_Server(int _num_clients, int portno)
+{
+    num_clients = _num_clients;
+
+    /* Initialize client socket descriptor vector */
+    if (num_clients < TCP_MAX_NUM_CLIENTS)
+    {
+        client_sock_fds = std::vector<int>(num_clients, 0);
+    }
+    else
+    {
+        client_sock_fds = std::vector<int>(TCP_MAX_NUM_CLIENTS, 0);
+    }
+
+    /* Create a master socket */
+	if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
+		perror("TCP SELECT SERVER ERR: socket");
+        throw FAILURE_CREATE_SOCKET; 
+	} 
+	
+	/* Allow server rerun immediately after being killed */   
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("TCP SELECT SERVER ERR: setsockopt"); 
+        throw FAILURE_SETSOCKOPT; 
+    }
+
+	/* Build the server's Internet address */
+    server_address.sin_family = AF_INET; 
+    server_address.sin_addr.s_addr = INADDR_ANY; 
+    server_address.sin_port = htons(portno); 
+       
+    /* Bind socket to port */
+    if (bind(server_fd, (struct sockaddr *)&server_address, 
+                                            sizeof(server_address)) < 0)
+    { 
+        perror("TCP SELECT SERVER ERR: bind"); 
+        throw FAILURE_BIND;
+    } 
+    
+    /* Allow 5 requests to queue up */
+    if (listen(server_fd, 5) < 0) 
+    { 
+        perror("TCP SELECT SERVER ERR: listen"); 
+        throw FAILURE_LISTEN;
+    }
+	addr_len = sizeof(server_address);
+}
+
+void TCP_Select_Server::monitor()
+{
+    /* Socket multiplexing setup */
+    FD_ZERO(&read_fds);
+    FD_SET(server_fd, &read_fds);
+
+    int max_sd = server_fd;
+        
+    /* Add child sockets to set */
+    for (int i = 0; i < num_clients; i++) 
+    { 
+        int sd = client_sock_fds[i];
+        if(sd > 0) FD_SET(sd, &read_fds);
+        if(sd > max_sd) max_sd = sd;
+    } 
+
+    /* Wait for an activity on one of the sockets */
+    int activity = select(max_sd + 1, &read_fds, NULL, NULL, NULL);
+    if ((activity < 0) && (errno != EINTR)) 
+    {
+        perror("TCP SELECT SERVER ERR: listen"); 
+        throw FAILURE_SELECT;
+    }
+}
+
+void TCP_Select_Server::add_sock()
+{
+    if (FD_ISSET(server_fd, &read_fds)) 
+    { 
+        int new_socket;
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&server_address,
+                                    (socklen_t*)&addr_len)) < 0) 
+        {
+            perror("TCP SELECT SERVER ERR: accept"); 
+            throw FAILURE_ACCEPT;
+        } 
+
+        std::cout << "New Socket: " << new_socket << "\n";
+    
+        /* Add new socket to vector of client sockets */
+        for (int i = 0; i < num_clients; i++) { 
+            if (client_sock_fds[i] == 0) {
+                client_sock_fds[i] = new_socket; 
+                break;
+            }
+        } 
+    }
+}
+
+bool TCP_Select_Server::is_socket_set(int sock_fd)
+{
+    return FD_ISSET(sock_fd, &read_fds);
+}
+
+std::vector<int> *TCP_Select_Server::get_client_sock_fds()
+{
+    return &client_sock_fds;
+}
+
+int TCP_Select_Server::read_from_sock(int sock_fd, char buffer[])
+{
+    int bytes_read = read(sock_fd, buffer, TCP_BUF_SZ);
+    if (bytes_read < 0)
+    { 
+        perror("TCP SELECT SERVER ERR: reading from socket"); 
+        throw FAILURE_READ_SOCK;
+    }
+    return bytes_read;
+}
+
+int TCP_Select_Server::write_to_sock(int sock_fd, char *msg, int size)
+{
+    int bytes_sent = send(sock_fd, msg, size, 0);
+    if (bytes_sent < 0)
+    { 
+        perror("TCP SELECT SERVER ERR: writing to socket"); 
+        throw FAILURE_WRITE_SOCK;
+    }
+    return bytes_sent;
+}
+
+void TCP_Select_Server::close_sock(int sock_fd)
+{
+    close(sock_fd);
+}
+
+/*===========================================================================
  * TCP server implementation
  *==========================================================================*/
 TCP_Server::TCP_Server(int portno)
@@ -21,7 +160,7 @@ TCP_Server::TCP_Server(int portno)
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
         perror("TCP SERVER ERR: socket"); 
-        exit(EXIT_FAILURE); 
+        throw FAILURE_CREATE_SOCKET; 
     } 
     
     /* Allow server rerun immediately after being killed */   
@@ -29,7 +168,7 @@ TCP_Server::TCP_Server(int portno)
                                                   &opt, sizeof(opt))) 
     { 
         perror("TCP SERVER ERR: setsockopt"); 
-        exit(EXIT_FAILURE); 
+        throw FAILURE_SETSOCKOPT; 
     }
 
     /* Build the server's Internet address */
@@ -42,14 +181,14 @@ TCP_Server::TCP_Server(int portno)
                                             sizeof(server_address)) < 0)
     { 
         perror("TCP SERVER ERR: bind"); 
-        exit(EXIT_FAILURE); 
+        throw FAILURE_BIND;
     } 
     
     /* Allow 5 requests to queue up */
     if (listen(server_fd, 5) < 0) 
     { 
         perror("TCP SERVER ERR: listen"); 
-        exit(EXIT_FAILURE); 
+        throw FAILURE_LISTEN;
     }
 }
 
@@ -60,17 +199,17 @@ void TCP_Server::accept_sock()
                         (socklen_t*)&addr_len))<0) 
     { 
         perror("TCP SERVER ERR: accept"); 
-        exit(EXIT_FAILURE); 
+        throw FAILURE_ACCEPT; 
     }
 }
 
-int TCP_Server::write_to_sock(std::string msg) 
+int TCP_Server::write_to_sock(char *msg, int size) 
 {
-    int bytes_sent = send(new_socket_fd, msg.data(), msg.length(), 0);
+    int bytes_sent = send(new_socket_fd, msg, size, 0);
     if (bytes_sent < 0)
     { 
-        perror("TCP SERVER ERR: write"); 
-        exit(EXIT_FAILURE); 
+        perror("TCP SERVER ERR: writing to socket"); 
+        throw FAILURE_WRITE_SOCK;
     }
     return bytes_sent;
 }
@@ -80,8 +219,8 @@ int TCP_Server::read_from_sock(char buffer[])
     int bytes_read = read(new_socket_fd, buffer, TCP_BUF_SZ);
     if (bytes_read < 0)
     { 
-        perror("TCP SERVER ERR: read"); 
-        exit(EXIT_FAILURE); 
+        perror("TCP SERVER ERR: reading from socket"); 
+        throw FAILURE_READ_SOCK;
     }
     return bytes_read;
 }
@@ -106,6 +245,7 @@ void TCP_Client::connect_to_server(std::string host, int portno)
     if (client_fd < 0) 
     {
         perror("TCP CLIENT ERR: socket");
+        throw FAILURE_CREATE_SOCKET;
     } 
 
     /* Get the server's DNS entry */
@@ -114,7 +254,7 @@ void TCP_Client::connect_to_server(std::string host, int portno)
     {
         std::cerr << "TCP CLIENT ERR: cannot connect to host '" << host
                   << "'\n'"; 
-        exit(EXIT_FAILURE);
+        throw FAILURE_CONNECT_TO_HOST;
     }
 
     /* Build the server's Internet address */
@@ -129,19 +269,19 @@ void TCP_Client::connect_to_server(std::string host, int portno)
                                                 sizeof(server_addr)) < 0) 
     {
         perror("TCP CLIENT ERR: connect");
-        exit(EXIT_FAILURE);
+        throw FAILURE_CREATE_CONNECTION;
     }
 }
 
 
-int TCP_Client::write_to_sock(std::string msg) 
+int TCP_Client::write_to_sock(char *msg, int size) 
 {
     /* Send message to the server */
-    int bytes_sent = write(client_fd, msg.data(), msg.length());
+    int bytes_sent = send(client_fd, msg, size, 0);
     if (bytes_sent < 0)
     {
         perror("TCP CLIENT ERR: writing to socket");
-        exit(EXIT_FAILURE);
+        throw FAILURE_WRITE_SOCK;
     }
     return bytes_sent;
 }
@@ -151,8 +291,8 @@ int TCP_Client::read_from_sock(char buffer[])
     int bytes_read = read(client_fd, buffer, TCP_BUF_SZ);
     if (bytes_read < 0)
     { 
-        perror("TCP SERVER ERR: read"); 
-        exit(EXIT_FAILURE); 
+        perror("TCP CLIENT ERR: reading from socket"); 
+        throw FAILURE_READ_SOCK; 
     }
     return bytes_read;
 }
--- src/tcp.h
+++ src/tcp.h
@@ -13,22 +13,68 @@
 #include <unistd.h> 
 #include <sys/socket.h> 
 #include <sys/types.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h>
-#include <netdb.h> 
+#include <netdb.h>
+#include <vector>
 
 #ifndef TCP_H_
 #define TCP_H_
 
-#define TCP_BUF_SZ 1024
+/* Constants */
+const int TCP_BUF_SZ = 1024;
+const int TCP_MAX_NUM_CLIENTS = 50;
 
-class TCP_Server {
+enum TCP_Exceptions {
+    FAILURE_SETSOCKOPT,
+    FAILURE_BIND,
+    FAILURE_LISTEN,
+    FAILURE_ACCEPT,
+    FAILURE_SELECT,
+    FAILURE_CREATE_SOCKET,
+    FAILURE_CONNECT_TO_HOST,
+    FAILURE_CREATE_CONNECTION,
+    FAILURE_WRITE_SOCK,
+    FAILURE_READ_SOCK
+};
+
+/* TCP_Select_Server class */
+class TCP_Select_Server 
+{
+    public:
+        TCP_Select_Server(int num_clients, int portno);
+        void monitor();
+        void add_sock();
+        int read_from_sock(int sock_fd, char buffer[]);
+        int write_to_sock(int sock_fd, char *msg, int size);
+        void close_sock(int sock_fd);
+        bool is_socket_set(int sock_fd);
+
+        /* Getters */
+        std::vector<int> *get_client_sock_fds();
+
+    private:
+        int server_fd;
+        int num_clients;
+        std::vector<int> client_sock_fds;
+        struct sockaddr_in server_address;
+        int opt = 1;
+        int addr_len;
+        fd_set read_fds;
+};
+
+/* TCP_Server class */
+class TCP_Server 
+{
     public:
         TCP_Server(int portno);
         void accept_sock();
         int read_from_sock(char buffer[]);
-        int write_to_sock(std::string msg);
+        int write_to_sock(char *msg, int size);
         void close_sock();
 
     private:
@@ -37,12 +83,15 @@ class TCP_Server {
         int opt = 1;
         int addr_len;
 };
-class TCP_Client {
+
+/* TCP_Client class */
+class TCP_Client 
+{
     public:
         TCP_Client();
         void connect_to_server(std::string host, int portno);
         int read_from_sock(char buffer[]);
-        int write_to_sock(std::string msg);
+        int write_to_sock(char *msg, int size);
         void close_sock();
 
     private:
