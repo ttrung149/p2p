c3d6a47819e5bc4dda4d1716e4dbe95ab4df0a1b
Trung Truong <ttruong91498@gmail.com>
2020-03-22 21:58:56 -0400

    Update send_file_to_peer function

--- src/peer.cpp
+++ src/peer.cpp
@@ -85,25 +85,32 @@ void Peer::start_server(std::string name, int port)
 }
 
 /**
- * Helper function: Check if socket has buffered enough data as 
+ * Helper function: Wait until socket has buffered enough data as 
  * specified
  * @param sock Passed-by-reference SockData object in client sockets
+ * @param server Passed-by-reference TCP_Select_Server
  * @param goal Amount of data that is required to be buffered
- * @param bytes_read Recently bytes read amount from socket
- * @param buffer Buffer array recently read from socket
- * @returns Returns true if the buffer size in SockData is equal to
- * or exceeds goal. Else, returns false.
+ * @returns void
  */
-static bool finished_buffering(SockData &sock, unsigned int goal, 
-                                unsigned int bytes_read, char buffer[])
+static void finish_buffering(SockData &sock, TCP_Select_Server &server,
+                                                        unsigned int goal)
 {
-    if (sock.bytes_read + bytes_read < goal)
+    while (sock.bytes_read < goal)
     {
-        memcpy(sock.buffer + sock.bytes_read, buffer, bytes_read);
-        sock.bytes_read += bytes_read;
-        return false;
+        char buffer[TCP_BUF_SZ];
+        int bytes = server.read_from_sock(sock.sock_fd, buffer);
+        if (bytes == 0) 
+        {
+            server.close_sock(sock.sock_fd);
+            sock.sock_fd = 0;
+            sock.bytes_read = 0;
+            bzero(sock.buffer, TCP_BUF_SZ);
+            return;
+        }
+
+        memcpy(sock.buffer + sock.bytes_read, buffer, bytes);
+        sock.bytes_read += bytes;
     }
-    return true;
 }
 
 /**
@@ -115,100 +122,65 @@ static bool finished_buffering(SockData &sock, unsigned int goal,
  */
 void Peer::handle_incoming_reqs(TCP_Select_Server &server, SockData &sock)
 {
-    char buffer[TCP_BUF_SZ] = {0};
-    int sd = sock.sock_fd;
-    int _bytes_read = server.read_from_sock(sd, buffer);
-
-    // Closes and reset socket when there's no more to read
-    if (_bytes_read == 0) 
-    {
-        server.close_sock(sd);
-        sock.sock_fd = 0;
-        sock.bytes_read = 0;
-        bzero(sock.buffer, TCP_BUF_SZ);
-    }
     // Continue buffer message until received at least 2 bytes (messsage type)
-    else if (!finished_buffering(sock, 2, _bytes_read, buffer)) return;
+    finish_buffering(sock, server, 2);
+
     // Parse received message
-    else 
+    char type_buf[2];
+    memcpy(type_buf, sock.buffer, 2);
+    message_type t = (message_type) ntohs(*(unsigned short *)(type_buf));
+
+    // Handle each incoming request to peer server
+    switch (t)
     {
-        // Update buffer in sock
-        memcpy(sock.buffer + sock.bytes_read, buffer, _bytes_read);
-        sock.bytes_read += _bytes_read;
-
-        char type_buf[2];
-        memcpy(type_buf, buffer, 2);
-        message_type t = (message_type) ntohs(*(unsigned short *)(type_buf));
-    
-        // Handle each incoming request to peer server
-        switch (t)
+        case REQ_PEER:
         {
-            case REQ_PEER:
-            {
-                if (!finished_buffering(sock, sizeof(ReqPeerMsg), 0, buffer)) 
-                {
-                    return;
-                }
-                std::cout << "req_peer\n";
-                
-                // Parse received peer file request message
-                ReqPeerMsg parsed;
-                parse_reqpeer_msg(sock.buffer, parsed);
+            // Continue buffering until received full req_peer message
+            finish_buffering(sock, server, sizeof(ReqPeerMsg));
+            ReqPeerMsg parsed;
+            parse_reqpeer_msg(sock.buffer, parsed);
 
-                // Send file to peer who is requesting the file
-                this->send_file_to_peer(parsed.leecher_ip, 
-                                    parsed.leecher_portno, parsed.file_name);
-                break;
-            }
-            
-            case DATA:
-            {
-                std::cout << "data\n";
-                // Continue buffering until received msg type and file size
-                if (!finished_buffering(sock, 6, 0, buffer)) return;
-                DataMsg parsed;
-                parse_data_msg(sock.buffer, parsed);
+            // Send file to peer who is requesting the file
+            this->send_file_to_peer(parsed.leecher_ip, 
+                                parsed.leecher_portno, parsed.file_name);
 
-                char *data_received = new char[parsed.file_size + 6];
-                memcpy(data_received, sock.buffer, sock.bytes_read);
+            server.close_sock(sock.sock_fd);
+            sock.sock_fd = 0;
+            sock.bytes_read = 0;
+            bzero(sock.buffer, TCP_BUF_SZ);
+            break;
+        }
+        case DATA:
+        {
+            // Continue buffering until received full data message
+            finish_buffering(sock, server, sizeof(DataMsg));
+            DataMsg parsed;
+            parse_data_msg(sock.buffer, parsed);
 
-                while (sock.bytes_read < parsed.file_size + 6)
-                {
-                    bzero(sock.buffer, TCP_BUF_SZ);
-                    int bytes = server.read_from_sock(sock.sock_fd, buffer);
-                    memcpy(data_received + sock.bytes_read, buffer, bytes);
-                    sock.bytes_read += bytes;
-                }
-
-                parse_data_msg(data_received, parsed);
-                server.close_sock(sd);
-                sock.sock_fd = 0;
-                sock.bytes_read = 0;
-                bzero(sock.buffer, TCP_BUF_SZ);
-
-                delete[] data_received;
-                break;
-            }
-            case ERR_FILE_NOT_FOUND:
-            {
-                std::cout << "err\n";
-                break;
-            }
-            case FILE_FOUND:
-            {
-                std::cout << "file found\n";
-                break;
-            }
-
-            default:
-            {
-                std::cout << "default\n";
-                server.close_sock(sd);
-                sock.sock_fd = 0;
-                sock.bytes_read = 0;
-                bzero(sock.buffer, TCP_BUF_SZ);
-                break;
-            }
+            server.close_sock(sock.sock_fd);
+            sock.sock_fd = 0;
+            sock.bytes_read = 0;
+            bzero(sock.buffer, TCP_BUF_SZ);
+            break;
+        }
+        case ERR_FILE_NOT_FOUND:
+        {
+            std::cout << "err\n";
+            break;
+        }
+        case FILE_FOUND:
+        {
+            std::cout << "file found\n";
+            break;
+        }
+        default:
+        {
+            std::cout << "default\n";
+            server.close_sock(sock.sock_fd);
+            sock.sock_fd = 0;
+            sock.bytes_read = 0;
+            bzero(sock.buffer, TCP_BUF_SZ);
+            break;
         }
     }
 }
@@ -237,31 +209,53 @@ void Peer::send_file_to_peer(std::string peer_host, int peer_port,
                                                     std::string file_name)
 {
     TCP_Client peer_client = TCP_Client();
-
-    // File handling
     std::ifstream requested_file(file_name, std::ios::binary);
     try
     {
         if (requested_file)
         {
             // Get file size
-            requested_file.seekg (0, requested_file.end);
+            requested_file.seekg(0, requested_file.end);
             int file_size = requested_file.tellg();
-            requested_file.seekg (0, requested_file.beg);
+            requested_file.seekg(0, requested_file.beg);
 
-            // Read in file to buffer
-            char *data = new char[file_size];
-            assert(data);
-            requested_file.read(data, file_size);
+            // Divide file into 512 bytes buffer chunks for data message
+            int segno;
+            for (segno = 0; segno < file_size / DATA_MSG_BUF_SIZE; segno++)
+            {
+                char data[DATA_MSG_BUF_SIZE] = {0};
+                requested_file.read(data, DATA_MSG_BUF_SIZE);
+                peer_client.connect_to_server(peer_host, peer_port);
 
-            // Send data message
-            char *data_msg = create_data_msg(file_size, data);
-            peer_client.connect_to_server(peer_host, peer_port);
-            peer_client.write_to_sock(data_msg, file_size + 6);
-            peer_client.close_sock();
+                DataMsg *m = create_data_msg(
+                    file_size, 
+                    DATA_MSG_BUF_SIZE, 
+                    segno, file_name, ip, portno, data
+                );
+                peer_client.write_to_sock((char *)m, sizeof(DataMsg));
+                delete m;
+                peer_client.close_sock();
+            }
             
-            delete[] data_msg;
-            delete[] data;
+            // Handle remaining file segment
+            int remaining_buf_sz = file_size % DATA_MSG_BUF_SIZE;
+            if (remaining_buf_sz != 0)
+            {
+                char data[DATA_MSG_BUF_SIZE] = {0};
+                requested_file.read(data, DATA_MSG_BUF_SIZE);
+                peer_client.connect_to_server(peer_host, peer_port);
+
+                DataMsg *m = create_data_msg(
+                    file_size,
+                    remaining_buf_sz,
+                    segno, file_name, ip, portno, data
+                );
+                peer_client.write_to_sock((char *)m, sizeof(DataMsg));
+                delete m;
+                peer_client.close_sock();
+            }
+
+            // File clean-up
             requested_file.close();
         }
         else
